4efacef615a548caa045574addfd99bbd1fdf8fe
Robert Chen <bergintonatcapital@gmail.com>
2023-05-31 15:15:57 -0400

    Implement logic for tic-tac-toe player vs player

--- script.js
+++ script.js
@@ -2,42 +2,73 @@ const gameBoard = (() => {
 	const row1 = [null, null, null];
 	const row2 = [null, null, null];
 	const row3 = [null, null, null];
+	const board = [row1, row2, row3];
+
+	const updateBoard = (target) => {
+		board[target.dataset.row][target.dataset.column] = target.textContent;
+	};
 	return {
-		board: [row1, row2, row3],
+		updateBoard,
+		board,
 	};
 })();
 
-// Takes in user inputs and outputs them into the gameBoard
+// Controls the logic of player clicks and outputs them into the gameBoard. Updates gameBoard with playerInput
 const controller = () => {
-	const _gameBoardSquares = document.querySelectorAll(".square");
-   _gameBoardSquares.forEach((square) => square.addEventListener('click', _playerVSplayer));
+	const _gameBoardSquares = document.querySelectorAll(".square").forEach((square) => square.addEventListener("click", _playerVSplayer));
+	const { updateBoard, board } = gameBoard;
+
+   function displayWinner(winner) {
+      const displayWinner = document.querySelector('.winner-display > span');
+      displayWinner.textContent = winner;
+   }
+
+   function checkWinner() {
+      // Check rows
+      for(let i = 0; i < 3; i++) {
+         if(board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2])
+            return board[i][0];
+      }
+      // Check columns
+      for(let j = 0; j < 3; j++) {
+         if(board[0][j] !==null && board[0][j] === board[1][j] && board[1][j] == board[2][j]) 
+            return board[0][j];
+      }
+      // Check diagonals
+      if(board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2])
+         return board[0][0];
+      if(board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0])
+         return board[2][0];
+
+      return null; //return null if no winners
+   }
+   
+	function _setPlayerInputX(event) {
+		event.target.textContent = "X";
+		updateBoard(event.target);
+	}
+	function _setPlayerInputY(event) {
+		event.target.textContent = "O";
+		updateBoard(event.target);
+	}
+
    let playerActive = false;
-
-   function _setPlayerInputX(event) {
-      event.target.textContent = 'X';
-      playerActive = false;
-   }
-   function _setPlayerInputY(event) {
-      event.target.textContent = 'Y';
-      playerActive = true;
-   }
-
 	function _playerVSplayer(event) {
-      if(event.target.textContent.length === 0)
-         if(playerActive) 
-            _setPlayerInputX(event);
-         else {
-            _setPlayerInputY(event);
+      // IF there's nothing in the board's square
+		if (event.target.textContent.length === 0)
+			if (playerActive) {
+				_setPlayerInputX(event);
+            playerActive = false;
+			} else {
+				_setPlayerInputY(event);
+            playerActive = true;
+			}
+         console.log(board)
+         if(checkWinner() !== null) {
+            const winner = checkWinner()
+            console.log(winner)
+            displayWinner(winner);
          }
-		// board.forEach((row) => {
-		// 	row.forEach((column) => {
-		// 		const div = document.createElement("div");
-		// 		div.classList.add("square");
-		// 		div.textContent = column;
-      //       div.addEventListener('click', _setPlayerInput)
-		// 		_gameBoardElement.appendChild(div);
-		// 	});
-		// });
 	}
 };
 
